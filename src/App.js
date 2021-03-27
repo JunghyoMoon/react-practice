b5dd6a7b1d32c2ebcfbcecb90094617e9df3cc8c
junghyomoon <dsinhome70@gmail.com>
2021-03-27 21:54:43 +0900

    basic usage of react

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+	state = {
+		hello: "hello app js! why react?",
+	};
+
+	handleClick = () => {
+		this.setState({
+			hello: "because it's cool!",
+		});
+	};
+
+	render() {
+		return (
+			<div className="App">
+				<div>{this.state.hello}</div>
+				<button onClick={this.handleClick}>beacause..</button>
+			</div>
+		);
+	}
 }
 
 export default App;
